a35eb4134f53f542706cab02d77fe4582b7dedb9
Graeme Fox <ac.graeme@gmail.com>
2022-09-25 21:49:04 -0600

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: './public/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
