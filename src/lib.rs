83bc83a10ec2cf808887a288759b6bea1df890a8
Graeme Fox <ac.graeme@gmail.com>
2022-09-27 20:09:06 -0600

    image load fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
@@ -8,4 +9,7 @@ pub fn grayscale(encoded_file: &str) {
 
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image Decoded".into());
+
+    let img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image Loaded".into())
 } 
